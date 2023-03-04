84ed428e6c79670896dd6d66e9bed626b95f097c
taharana <m.taha.1213@gmail.com>
2023-03-04 19:51:53 +0500

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\CompanyController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('acme',[CompanyController::class,'acme'])->name('acme');
+Route::get('count',[CompanyController::class,'count'])->name('count');
+
+
